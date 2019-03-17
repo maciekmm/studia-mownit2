5aeb27d3fac64abf66e6e7f4fcafe8081db31477
Maciej Mionskowski <business@maciekmm.net>
2019-03-17 11:12:36 +0100

    Fix trajectory diagram in exercise 01

--- lab-01/src/pl/mionskowski/mownit/lab01/Bifurcation.java
+++ lab-01/src/pl/mionskowski/mownit/lab01/Bifurcation.java
@@ -2,17 +2,17 @@ package pl.mionskowski.mownit.lab01;
 
 public class Bifurcation {
     public static final float[] XS = new float[]{0.15f, 0.3f, 0.2435f, 0.9f, 0.421f};
-    public static final float R_STEP = 0.01f;
+    public static final float R_STEP = 0.00001f;
 //    public static final float R_MIN = 1;
     public static final float R_MIN = 3.75f;
     public static final float R_MAX = 3.8f;
-    public static final int ITERATIONS = 1 << 12;
+    public static final int ITERATIONS = 1 << 10;
     public static final int ITERATION_THRESHOLD = 1 << 3;
-    public static final float DIFF_EPS = 0.0002f;
+    public static final float DIFF_EPS = 0.00002f;
 
     public static void main(String[] args) {
         for (float r = R_MIN; r < R_MAX; r += R_STEP) {
-            print(r, XS[2]);
+            print(r, 0.4523f);
         }
     }
 
